5c7b9409ef368cf4837af8948cf87273ac46f8e0
daniel lepham <daniel.lepham@gm.com>
2020-11-12 06:14:10 -0600

    fixed docker registry repo name

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-docker build -t leinadmahpel/multi-client:latest -t leinadmahpel/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t leinadmahpel/multi-server:latest -t leinadmahpel/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t leinadmahpel/multi-worker:latest -f -t leinadmahpel/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker build -t danwithaplan112/multi-client:latest -t danwithaplan112/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t danwithaplan112/multi-server:latest -t danwithaplan112/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t danwithaplan112/multi-worker:latest -f -t danwithaplan112/multi-worker:$SHA ./worker/Dockerfile ./worker
 
-docker push leinadmahpel/multi-client:latest
-docker push leinadmahpel/multi-server:latest
-docker push leinadmahpel/multi-worker:latest
+docker push danwithaplan112/multi-client:latest
+docker push danwithaplan112/multi-server:latest
+docker push danwithaplan112/multi-worker:latest
 
-docker push leinadmahpel/multi-client:$SHA
-docker push leinadmahpel/multi-server:$SHA
-docker push leinadmahpel/multi-worker:$SHA
+docker push danwithaplan112/multi-client:$SHA
+docker push danwithaplan112/multi-server:$SHA
+docker push danwithaplan112/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=leinadmahpel/multi-server:$SHA
-kubectl set image deployments/client-deployment client=leinadmahpel/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=leinadmahpel/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=danwithaplan112/multi-server:$SHA
+kubectl set image deployments/client-deployment client=danwithaplan112/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=danwithaplan112/multi-worker:$SHA
